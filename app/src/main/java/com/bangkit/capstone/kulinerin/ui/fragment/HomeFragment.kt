b982d6889c49787d72246cb2acd8c006a4433dc7
dduuddeekk <jajajablablabla123@gmail.com>
2024-11-22 13:08:04 +0800

    Implementing binding and Home.

--- app/src/main/java/com/bangkit/capstone/kulinerin/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/bangkit/capstone/kulinerin/ui/fragment/HomeFragment.kt
@@ -5,24 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bangkit.capstone.kulinerin.R
+import com.bangkit.capstone.kulinerin.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
