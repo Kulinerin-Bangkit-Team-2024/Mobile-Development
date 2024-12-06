b5d1c4d9f20c48f9f0ef5ee3f0c762adec941fb2
dduuddeekk <jajajablablabla123@gmail.com>
2024-12-06 13:57:48 +0800

    Service.

--- app/src/main/java/com/bangkit/capstone/kulinerin/data/response/LogOutResponse.kt
+++ app/src/main/java/com/bangkit/capstone/kulinerin/data/response/LogOutResponse.kt
@@ -2,23 +2,11 @@ package com.bangkit.capstone.kulinerin.data.response
 
 import com.google.gson.annotations.SerializedName
 
-data class RegisterResponse(
+data class LogOutResponse(
 
 	@field:SerializedName("message")
 	val message: String,
 
-	@field:SerializedName("user")
-	val user: UserRegister,
-
 	@field:SerializedName("status")
 	val status: String
-)
-
-data class UserRegister(
-
-	@field:SerializedName("name")
-	val name: String,
-
-	@field:SerializedName("email")
-	val email: String
 )
